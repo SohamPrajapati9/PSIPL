6a023c52253610106fc14fdd8fe86c01219927a6
Soham Prajapati <9.sohamprajapati@gmail.com>
2024-10-03 09:50:02 +0530

    Add files via upload

---
+++ Assign_op.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+int x=10;
+printf("The value of x is %d\t \n", x+=5);
+printf("The value of x is %d\t \n", x-=5);
+printf("The value of x is %d\t \n", x*=5);
+printf("The value of x is %d\t \n", x/=5);
+printf("The value of x is %d\t \n", x%=5);
+return 0;
+}
