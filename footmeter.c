035afbc44e3b0473bc015861cf5e2f91d904d83f
Soham Prajapati <9.sohamprajapati@gmail.com>
2024-10-03 09:19:12 +0530

    Add files via upload

---
+++ footmeter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+ float foot,meter;
+ printf("Enter the value in foot \n");
+ scanf("%f", &foot);
+ meter = 0.3*foot;
+ printf("%f", meter);
+ printf("meter");
+ return 0;
+ }
---
+++ indrofb.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+
+int main()
+{
+ int a=10,b;
+ b=a++;
+ printf("%d", b);
+ b=++a;
+ printf("%d", b);
+ b=a--;
+ printf("%d", b);
+ b=--a;
+ printf("%d", b);
+ return 0;
+ }
