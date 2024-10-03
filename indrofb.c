0b0da35867d76ab27665905db9d43108f4470fa2
Soham Prajapati <9.sohamprajapati@gmail.com>
2024-10-03 09:51:03 +0530

    indrofb.c

--- indrofb.c
+++ indrofb.c
@@ -4,12 +4,12 @@ int main()
 {
  int a=10,b;
  b=a++;
- printf("%d", b);
+ printf("%d \n", b);
  b=++a;
- printf("%d", b);
+ printf("%d \n", b);
  b=a--;
- printf("%d", b);
+ printf("%d \n", b);
  b=--a;
- printf("%d", b);
+ printf("%d \n", b);
  return 0;
  }
