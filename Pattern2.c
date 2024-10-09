210da53c4a236beaa9dc5443f28523d9869699bc
Soham Prajapati <9.sohamprajapati@gmail.com>
2024-10-09 14:19:57 +0530

    Update Pattern2.c

--- Pattern2.c
+++ Pattern2.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main(){
-int num;
-scanf("%d",&num);
-  
-for(int i=num;i>0;i--){
-for(int j=i;j>0;j--){
-printf("%d ",j);
-}
-printf(" \n");
-}
 
-return 0;
+  printf(" Write a number for size \n ");
+  int num;
+  scanf("%d",&num);
+  
+  for(int i=num;i>0;i--){
+    for(int j=i;j>0;j--){
+      printf("%d ",j);
+    }
+    printf(" \n");
+  }
+  return 0;
 }
