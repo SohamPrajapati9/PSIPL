cea2d553d956cb064160f712ffe5dc6264cda4bb
Soham Prajapati <9.sohamprajapati@gmail.com>
2024-10-26 10:03:58 +0530

    Update Recursion3.c

--- Recursion3.c
+++ Recursion3.c
@@ -4,13 +4,17 @@ int fib(int n){
    if(n==1 || n==0){
       return n;
    }
-   return fib(n-1) + fib(n-2);
+   return (fib(n-1) + fib(n-2));
 }
 
 int main(){
    int n;
    printf("Enter a number \n");
    scanf("%d",&n);
-   fib(n);
+   for(int i=1;i<=n;i++){
+      int p = fib(n);
+      printf("%d \t",p);
+   }
+   printf(" \n ");
    return 0;
 }
