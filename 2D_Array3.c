1123fc76ce81c36606797826eebd2ba1300b162d
Soham Prajapati <9.sohamprajapati@gmail.com>
2024-11-06 08:44:03 +0530

    Update 2D_Array3.c

--- 2D_Array3.c
+++ 2D_Array3.c
@@ -1,13 +1,12 @@
 #include <stdio.h> 
 #include <math.h>
-#include <conio.h>
 
 int main(){
    int n;
    printf("Write the dimensions of the 2D Array \n");
    scanf("%d",&n);
    int a1[n][n];
-   printf("Enter the elements for first array\n");
+   printf("Enter the elements for array\n");
    for(int i=0;i<n;i++){
       printf("Enter the elements for %d row \n",i);
       for(int j=0;j<n;j++){
